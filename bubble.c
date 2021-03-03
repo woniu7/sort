ba580ddc0147fa754536ed2ab9ee0855fd193043
rongda.wang <rongda@mys4s.cn>
2021-03-03 14:23:55 +0800

    bubble improve

--- bubble.c
+++ bubble.c
@@ -1,7 +1,8 @@
-void bubble(int *arr, int len) {
+int bubble(int *arr, int len) {
+    int dot = 0;
     for (int i = 0; i < len-1; i++) {
-        for (int j = 0; j < len-1-i; j++) {
-            if (arr[j] > arr[j+1])
+        for (int j = 0; j < len-1; j++) {
+            if (dot++, arr[j] > arr[j+1])
             {
                 arr[j] = arr[j]^arr[j+1];
                 arr[j+1] = arr[j]^arr[j+1];
@@ -9,4 +10,62 @@ void bubble(int *arr, int len) {
             }
         }
     }
+    return dot;
+}
+
+int bubble1(int *arr, int len) {
+    int dot = 0;
+    for (int i = 0; i < len-1; i++) {
+        for (int j = 0; j < len-1-i; j++) {
+            if (dot++, arr[j] > arr[j+1])
+            {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+    return dot;
+}
+
+int bubble2(int *arr, int len) {
+    int dot = 0;
+    for (int i = 0; i < len-1; i++) {
+        int isSwap = 0;
+        for (int j = 0; j < len-1-i; j++) {
+            if (dot++, arr[j] > arr[j+1])
+            {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                isSwap = 1;
+            }
+        }
+        if (isSwap == 0) { 
+            break;
+        }
+    }
+    return dot;
+}
+
+int bubble3(int *arr, int len) {
+    int dot = 0;
+    int endIndex = len;
+    for (int i = 0; i < len-1; i++) {
+        int isSwap = 0;
+        for (int j = 0, end = endIndex; j < end-1; j++) {
+            if (dot++, arr[j] > arr[j+1])
+            {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                endIndex = j+1;
+                isSwap = 1;
+            }
+        }
+        if (isSwap == 0) { 
+            break;
+        }
+    }
+    return dot;
 }
--- main.c
+++ main.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 
-void bubble(int *, int);
-void quick(int *, int);
-void pick(int *, int);
-void heap(int *, int);
+#define FUNC_DEF(func) { func, #func },
 
-void sort(void (*f)(int *,int), int* arr, int len)
+int bubble(int *, int);
+int bubble1(int *, int);
+int bubble2(int *, int);
+int bubble3(int *, int);
+
+int quick(int *, int);
+int pick(int *, int);
+int heap(int *, int);
+
+void sort(int (*f)(int *,int), int* arr, int len)
 {
-        f(arr, len);
+        int dot = f(arr, len);
+	printf("\tdot: %d \t", dot);
 }
 
 int main(int argc, char **argv)
 {
-    void* goods[] = {bubble};
+    //void* goods[] = {bubble, bubble1, bubble2, bubble3};
+    struct {
+        void* func;
+        const char * name;
+    } goods[] = {
+        FUNC_DEF(bubble)
+        FUNC_DEF(bubble1)
+        FUNC_DEF(bubble2)
+        FUNC_DEF(bubble3)
+    };
 
-    for (int j = 0; j < 1; j++) 
+    for (int j = 0; j < sizeof(goods)/(2*sizeof(void*)); j++) 
     {
-	    int arr[] = {7, 4, 5, 7, 6, 2, 0, 4, 1, 3};
+	    int arr[] = {7, 4, 5, 7, 6, 2, 0, 4, 1, 3, 1000, 1001, 1002, 1003, 1004, 1005, 1005, 1006, 1007};
 	    int len =  sizeof(arr) / sizeof(int);
+	    //for (int i = 0; i < len; i++) printf("%d ", arr[i]);
+	    //printf("\n");
+
+            printf("%s\n", goods[j].name);
+	    // sort
+	    sort(goods[j].func, arr, len);
+
 	    for (int i = 0; i < len; i++) printf("%d ", arr[i]);
 	    printf("\n");
-
-	    // sort
-	    sort(goods[j], arr, len);
-
-	    for (int i = 0; i < len; i++) printf("%d ", arr[i]);
     }
 }
